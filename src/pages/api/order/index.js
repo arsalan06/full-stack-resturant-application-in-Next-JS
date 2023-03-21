f6e84f89a246a2e257ca603427884fb78fc7f7d4
arsalan_qadir <arsalan.qadir@otwosoft.com>
2023-03-21 09:12:15 +0500

    add strip successfully

--- src/pages/api/order/index.js
+++ src/pages/api/order/index.js
@@ -1,7 +1,24 @@
+import Order from "@/models/Order";
 import dbConnect from "@/util/mongoos";
 import Product from "../../../models/Product";
 export default async function handler(req, res) {
   const { method } = req;
-  if(method=="GET"){}
-  if(method=="POST"){}
+  if(method=="GET"){
+    try{
+        const order =await Order.find(req.body);
+        res.status(201).json(order)
+      }catch(err){
+          res.status(500).json(err)
+      }
+  }
+  if(method=="POST"){
+    try{
+      const order =await Order.create(req.body);
+      res.status(201).json(order)
+    }catch(err){
+        res.status(500).json(err)
+    }
+  }
+  if(method=="PUT"){}
+  if(method=="DELETE"){}
 }
